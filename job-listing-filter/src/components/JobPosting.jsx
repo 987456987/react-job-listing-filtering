c5c3ae8ace4f5a25292d8a2fceaf74962518bace
987456987 <donufrak112697@gmail.com>
2023-09-10 09:37:14 -0700

    posting html layout

--- job-listing-filter/src/components/JobPosting.jsx
+++ job-listing-filter/src/components/JobPosting.jsx
@@ -1,7 +1,20 @@
 function JobPosting ( {item} ) {
     return (
         <>
-
+            <div className="main-container">
+                <div className="edge"></div>
+                <div className="content-container">
+                    <div className="left-container">
+                        <img className="logo" />
+                        <div className="info-container">
+                            <div className="top-container"></div>
+                            <div className="mid-container"></div>
+                            <div className="bottom-container"></div>
+                        </div>
+                    </div>
+                    <div className="right-container"></div>
+                </div>
+            </div>
         </>
     )
 }
